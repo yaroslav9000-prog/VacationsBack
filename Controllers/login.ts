ffc8f29a2998a24c8b5e6824c86e495eee46ab53
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-08-11 17:57:04 +0300

    solved problem with dotenv

--- Controllers/login.ts
+++ Controllers/login.ts
@@ -1,16 +1,38 @@
 import {Response, Request} from "express";
 import { fetchUsers } from "../src/resources/user/users.controller";
 import { User } from "../src/resources/user/user";
-
+import jwt from "jsonwebtoken";
+import { userModel } from "../src/resources/user/User.model";
+import { ObjectId } from "mongodb";
+const result = require("dotenv").config();
+const env = result.parsed;
 const handleLogin = async (req: Request, res: Response)=>{
     const {email} = req.body;
     const usersData = await fetchUsers();
-    const duplicateEmail = usersData.find((item: User)=> item.email == email);
+    // const duplicateEmail = usersData.find((item: User)=> item.email == email);
     if(!req.body.pwd || req.body.firstName) return res.status(400).json({"msg": "password and username is required"});
     const foundUser = usersData.find((item: User)=> item.email == req.body.email);
     if(foundUser && foundUser.pwd === req.body.pwd){
         //Before i finish i generate a jwt for a user.
-        return res.status(200).json({"msg": "access granted"});
+        const jwtPayload = {
+            id: foundUser._id,
+            name: foundUser.firstName,
+            role: foundUser.role
+        }
+        const accessTokenSecret = env["ACCESS_TOKEN_SECRET"];
+        const refreshTokenSecret = env["REFRESH_TOKEN_SECRET"];
+        const accessToken = jwt.sign(
+            jwtPayload,
+            accessTokenSecret,
+            {expiresIn: "30s"}
+        );
+        const refreshToken = jwt.sign(
+            {"email": foundUser.email},
+            refreshTokenSecret,
+            {expiresIn: "1d"}
+        );
+        userModel.findByIdAndUpdate()}, {$set: {"refreshToken": refreshToken}})
+        res.json({accessToken});
     }else{
         return res.sendStatus(401);
     }
--- Routes/authorization.ts
+++ Routes/authorization.ts
@@ -7,7 +7,7 @@ export const authorizationRouter = Router();
 authorizationRouter.get("/login", async(req: Request, res: Response)=>{
     try{
         handleLogin(req, res)
-        console.log('I fetched you the users');
+        
     }catch(e){
         console.log(`mistake occured while you fetching data`);
         res.status(500).json('you fucked up boy');
